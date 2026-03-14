2a119dbaead49d2b4faa074ec577d4ecb2722878
rizaptk <rizaptk@outlook.com>
2026-03-15 06:48:00 +0700

    Create composite_index.rs
    new

---
+++ src/composite/composite_index.rs
@@ -0,0 +1,89 @@
+use std::collections::BTreeMap;
+
+use crate::document::firelite_doc::{FireLiteDoc, Value};
+use super::definition::CompositeIndexDefinition;
+use super::key_encoder::encode_composite_key;
+
+pub struct CompositeIndex {
+
+    pub definition: CompositeIndexDefinition,
+
+    tree: BTreeMap<Vec<u8>, String>,
+}
+
+impl CompositeIndex {
+
+    pub fn new(definition: CompositeIndexDefinition) -> Self {
+
+        Self {
+            definition,
+            tree: BTreeMap::new(),
+        }
+    }
+
+    pub fn index_document(
+        &mut self,
+        doc_id: &str,
+        doc: &FireLiteDoc,
+    ) {
+
+        let mut values = Vec::new();
+
+        for field in &self.definition.fields {
+
+            if let Some(v) = doc.fields.get(&field.field) {
+                values.push(v.clone());
+            } else {
+                return;
+            }
+        }
+
+        let key =
+            encode_composite_key(
+                &self.definition,
+                &values,
+                doc_id
+            );
+
+        self.tree.insert(key, doc_id.to_string());
+    }
+
+    pub fn remove_document(
+        &mut self,
+        doc_id: &str,
+        doc: &FireLiteDoc,
+    ) {
+
+        let mut values = Vec::new();
+
+        for field in &self.definition.fields {
+
+            if let Some(v) = doc.fields.get(&field.field) {
+                values.push(v.clone());
+            } else {
+                return;
+            }
+        }
+
+        let key =
+            encode_composite_key(
+                &self.definition,
+                &values,
+                doc_id
+            );
+
+        self.tree.remove(&key);
+    }
+
+    pub fn range_scan(
+        &self,
+        start: Vec<u8>,
+        end: Vec<u8>,
+    ) -> Vec<String> {
+
+        self.tree
+            .range(start..=end)
+            .map(|(_,doc)| doc.clone())
+            .collect()
+    }
+}
