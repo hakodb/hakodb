0c1276751ab974019bb76f52b0f5c4e9b5ec3fb7
rizaptk <rizaptk@outlook.com>
2026-03-15 07:43:31 +0700

    Create allocator.rs
    new

---
+++ src/memory/allocator.rs
@@ -0,0 +1,28 @@
+use std::sync::atomic::{AtomicUsize,Ordering};
+
+pub struct Allocator {
+
+    offset:AtomicUsize,
+
+}
+
+impl Allocator {
+
+    pub fn new()->Self{
+
+        Self{
+            offset:AtomicUsize::new(0)
+        }
+
+    }
+
+    pub fn allocate(
+        &self,
+        size:usize
+    )->usize{
+
+        self.offset.fetch_add(size,Ordering::SeqCst)
+
+    }
+
+}
