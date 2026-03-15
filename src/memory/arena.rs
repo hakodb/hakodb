7b315ccdb1ce4c1f97ae594dc1fc4c587069a8ef
rizaptk <rizaptk@outlook.com>
2026-03-15 07:27:07 +0700

    Create arena.rs
    new

---
+++ src/memory/arena.rs
@@ -0,0 +1,45 @@
+pub struct Arena {
+
+    buffer:Vec<u8>,
+
+    offset:usize,
+
+}
+
+impl Arena {
+
+    pub fn new(size:usize)->Self{
+
+        Self{
+            buffer:vec![0;size],
+            offset:0
+        }
+
+    }
+
+    pub fn alloc(
+        &mut self,
+        size:usize
+    )->Option<&mut [u8]>{
+
+        if self.offset+size > self.buffer.len(){
+            return None
+        }
+
+        let start = self.offset;
+
+        self.offset+=size;
+
+        Some(
+            &mut self.buffer[start..start+size]
+        )
+
+    }
+
+    pub fn reset(&mut self){
+
+        self.offset=0;
+
+    }
+
+}
